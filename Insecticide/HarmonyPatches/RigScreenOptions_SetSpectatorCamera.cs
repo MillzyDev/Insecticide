3b131d1d834329acf276edc0861b4746ce3dbc08
Checkerboard <Colehennessy1@gmail.com>
2024-08-05 17:54:54 -0700

    Formatting changes

--- Insecticide/HarmonyPatches/RigScreenOptions_SetSpectatorCamera.cs
+++ Insecticide/HarmonyPatches/RigScreenOptions_SetSpectatorCamera.cs
@@ -5,12 +5,10 @@ using Il2CppSLZ.Bonelab;
  * Fixes the skybox rendering in the spectator camera by setting the clear flags to "Skybox" instead of "Nothing".
  */
 
-namespace Insecticide.HarmonyPatches
-{
+namespace Insecticide.HarmonyPatches {
     [HarmonyPatch(typeof(RigScreenOptions))]
     [HarmonyPatch(nameof(RigScreenOptions.SetSpectatorCamera))]
-    internal static class RigScreenOptions_SetSpectatorCamera
-    {
+    internal static class RigScreenOptions_SetSpectatorCamera {
         [HarmonyPostfix]
         // ReSharper disable once InconsistentNaming
         // ReSharper disable once UnusedMember.Local
