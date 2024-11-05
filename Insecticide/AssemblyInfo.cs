04cb5024a904c70d505c8ffd8ea1fe5d63236a3c
Millz <millzyyt08@gmail.com>
2024-11-05 12:07:32 +0000

    bump melon version required

--- Insecticide/AssemblyInfo.cs
+++ Insecticide/AssemblyInfo.cs
@@ -5,5 +5,5 @@ using BuildInfo = Insecticide.BuildInfo;
 [assembly: MelonInfo(typeof(Mod), BuildInfo.Name, BuildInfo.Version, BuildInfo.Author)]
 [assembly: MelonGame("Stress Level Zero", "BONELAB")]
 [assembly: MelonID(BuildInfo.Id)]
-[assembly: VerifyLoaderVersion("0.6.4")]
+[assembly: VerifyLoaderVersion("0.6.5")]
 [assembly: HarmonyDontPatchAll]
